9f749218c9633bf552ae14f99a1afb43c18ab80c
Marcel Hauf <marcel.hauf@n-fuse.de>
2016-05-10 11:30:36 +0200

    Add server documentation, improve logging

--- example/user.js
+++ example/user.js
@@ -1,16 +1,6 @@
 'use strict';
 
-const Nanosecond = 1;
-const Microsecond = 1000 * Nanosecond;
-const Millisecond = 1000 * Microsecond;
-const Second = 1000 * Millisecond;
-const Minute = 60 * Second;
-const Hour = 60 * Minute;
-
-
 let co = require('co');
-let util = require('util');
-
 let Server = require('../lib/microservice').Server
 
 // Service the business logic of this microservice.
@@ -70,7 +60,7 @@ let config = {
   events: {
     provider: {
       name: 'kafka',
-      config: {
+      config: { // config object pased to kafka provider
         groupId: 'restore-chassis-example-server',
         clientId: 'restore-chassis-example-server',
         connectionString: 'localhost:9092',
@@ -78,7 +68,10 @@ let config = {
     },
   },
   endpoints: {
+    // TODO A default for all endpoints?
+    // eg.: transport: ['grpc'],
     activate: {
+      // Specify which configured transport the endpoint should use
       transport: ['grpc'],
     },
     changePassword: {
@@ -94,9 +87,11 @@ let config = {
       transport: ['grpc'],
     },
   },
+  // It is possible to register multiple transports of the same kind(name).
+  // For example a grpc server listening on port 50051 and one on port 50052.
   transports: [{
     name: 'grpc',
-    config: {
+    config: { // config object passed to grpc provider
       proto: '/../protos/user.proto',
       package: 'user',
       service: 'User',
@@ -105,6 +100,7 @@ let config = {
   }, ],
 };
 
+// makeLogging returns a simple middleware which is called before the business logic.
 function makeLogging(logger) {
   return function*(req) {
     logger.log('INFO', req.transport, req.method, req.request);
@@ -113,11 +109,22 @@ function makeLogging(logger) {
 }
 
 co(function*() {
+  // Create a new microservice Server
   let server = new Server(config);
+
+  // Add middleware
   server.middleware.push(makeLogging(server.logger));
+
+  // Subscribe to events which the business logic requires
   let userEvents = yield server.events.subscribe('user');
+
+  // Create the business logic
   let service = new Service(userEvents, server.logger);
+
+  // Bind business logic to server
   yield server.bind(service);
+
+  // Start server
   yield server.start();
 }).catch(function(err) {
   console.log(err.stack);
