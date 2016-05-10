066770224240a1c10be2e975813ede897e7c8bb6
Marcel Hauf <marcel.hauf@n-fuse.de>
2016-05-10 13:40:08 +0200

    Convert email example to new microservice Server chassis

--- example/email.js
+++ example/email.js
@@ -2,42 +2,12 @@
 
 let co = require('co');
 let util = require('util');
+let Server = require('../lib/microservice').Server
 
-// chassis
-
-// events
-let Events = require('../lib/transport/events/events').Events;
-let Kafka = require('../lib/transport/events/kafka').Kafka;
-
-let logger = {
-  log: console.log,
-};
-let options = {
-  logger: logger,
-  kafka: {
-    groupId: 'restore-chassis-example-email',
-    clientId: 'restore-chassis-example-email',
-    connectionString: 'localhost:9092',
-    logger: {
-      logFunction: logger.log,
-    },
-  },
-  timeout: 10000,
-};
-
-function sendEmail(mail) {
-  logger.log('debug', 'pretending to send email', mail);
-}
-
-function* init(options) {
-  // Events
-  // Create a Kafka provider
-  let kafka = new Kafka(options.kafka);
-  // Use Kafka provider for events
-  let events = new Events(kafka);
-  // subscribe to user events provided by kafka (topic:user)
-  let userEvents = yield events.subscribe('user');
-  // listen to user.created events (kafka messages)
+function Service(userEvents, logger) {
+  function* sendEmail(mail) {
+    logger.log('debug', 'pretending to send email', mail);
+  }
   userEvents.on('created', function*(message){
     let name = message.name || message.id;
     let msg = util.format('Hello user %s Your account has beeen created.', name);
@@ -45,7 +15,7 @@ function* init(options) {
       to: message.email,
       body: msg,
     };
-    sendEmail(email);
+    yield sendEmail(email);
   });
   userEvents.on('activated', function*(message){
     let name = message.name || message.id;
@@ -54,7 +24,7 @@ function* init(options) {
       to: message.email,
       body: msg,
     };
-    sendEmail(email);
+    yield sendEmail(email);
   });
   userEvents.on('deleted', function*(message){
     let name = message.name || message.id;
@@ -63,26 +33,39 @@ function* init(options) {
       to: message.email,
       body: msg,
     };
-    sendEmail(email);
-  });
-  // start provider kafka (consumer, producer)
-  yield kafka.start();
-
-  process.on('SIGINT', function() {
-    logger.log('signal', 'SIGINT');
-    co(function*(){
-      yield kafka.end();
-      process.exit(0);
-    }).catch(function(err){
-      logger.log('error', err);
-      process.exit(1);
-    });
+    yield sendEmail(email);
   });
 }
 
+let config = {
+  events: {
+    provider: {
+      name: 'kafka',
+      config: { // config object pased to kafka provider
+        groupId: 'restore-chassis-example-email',
+        clientId: 'restore-chassis-example-email',
+        connectionString: 'localhost:9092',
+      },
+    },
+  }
+};
+
 co(function*(){
-  let ms = yield init(options);
-}).catch(function(err){
-  logger.log('error', 'microservice', 'message', err);
+  // Create a new microservice Server
+  let server = new Server(config);
+
+  // Subscribe to events which the business logic requires
+  let userEvents = yield server.events.subscribe('user');
+
+  // Create the business logic
+  let service = new Service(userEvents, server.logger);
+
+  // Bind business logic to server
+  yield server.bind(service);
+
+  // Start server
+  yield server.start();
+}).catch(function(err) {
+  console.log(err.stack);
   process.exit(1);
 });
