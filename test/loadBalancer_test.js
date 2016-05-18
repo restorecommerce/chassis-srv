09876198e40e6e8fe8b435103a35a55b807a300d
Marcel Hauf <marcel.hauf@n-fuse.de>
2016-05-18 11:47:05 +0200

    Add roundRobin loadbalancer test

---
+++ test/loadBalancer_test.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var mocha = require('mocha')
+var coMocha = require('co-mocha')
+coMocha(mocha)
+
+var assert = require('assert');
+var should = require('should');
+var util = require('util');
+var isGeneratorFn = require('is-generator').fn
+var loadBalancer = require('../lib/loadbalancer');
+
+function* endpoint(request, context){
+  return yield {result:'ok'};
+}
+
+var tests = [
+  {
+    name: 'random',
+    loadBalancer: function(publisher) {
+      return loadBalancer.random(publisher, 1);
+    }
+  },
+  {
+    name: 'roundRobin',
+    loadBalancer: function(publisher) {
+      return loadBalancer.roundRobin(publisher);
+    }
+  }
+]
+
+tests.forEach(function(test){
+  console.log(test);
+  describe(util.format('%s loadBalancer', test.name), function () {
+    let zeroEndpoints = [];
+    let oneEndpoints = [endpoint];
+    let endpoints = [endpoint, endpoint, endpoint];
+
+    describe('with fixedPublisher and three endpoints, calling next', function () {
+      let publisher = loadBalancer.fixedPublisher(endpoints);
+      let random = test.loadBalancer(publisher);
+      let r = random.next();
+      it('should not end the loadBalancer', function* (){
+        assert.ok(!r.done);
+      });
+      it('should return one endpoint promise', function* (){
+        assert(r.value);
+        assert(r.value.then);
+      });
+      it('the endpoint should be a generator function and calling it should return a result', function* (){
+        // yield r.value because it is a promise
+        let endpoint = yield r.value;
+        assert.ok(isGeneratorFn(endpoint));
+        let result = yield endpoint({},{});
+        result.should.have.property('result', 'ok');
+      });
+    });
+
+    describe('with fixedPublisher and one endpoint, calling next', function () {
+      let publisher = loadBalancer.fixedPublisher(oneEndpoints);
+      let random = test.loadBalancer(publisher);
+      let r = random.next();
+      it('should not end the loadBalancer', function* (){
+        assert.ok(!r.done);
+      });
+      it('should return one endpoint promise', function* (){
+        assert(r.value);
+        assert(r.value.then);
+      });
+      it('the endpoint should be a generator function and calling it should return a result', function* (){
+        // yield r.value because it is a promise
+        let endpoint = yield r.value;
+        assert.ok(isGeneratorFn(endpoint));
+        let result = yield endpoint({},{});
+        result.should.have.property('result', 'ok');
+      });
+    });
+
+    describe('with fixedPublisher and zero endpoint, calling next', function () {
+      let publisher = loadBalancer.fixedPublisher(zeroEndpoints);
+      let random = test.loadBalancer(publisher);
+      it('should throw an error', function* (){
+        var fn = function*(){
+          let r = random.next();
+          let endpoint = yield r.value;
+        };
+        assert.throws(fn().next, 'publisher did not return endpoints');
+      });
+    });
+  });
+});
