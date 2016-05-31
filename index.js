b886a8b7bd92768ebade3e3d45f0e8130105e861
Marcel Hauf <marcel.hauf@n-fuse.de>
2016-05-31 11:32:06 +0200

    Fix require paths in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports.config = require('./lib/config');
 module.exports.loadbalancer = require('./lib/loadbalancer');
 module.exports.microservice = require('./lib/microservice');
-module.exports.grpc = require('./lib/transport/grpc');
+module.exports.grpc = require('./lib/transport/provider/grpc');
 module.exports.endpoint = require('./lib/endpoint');
-module.exports.events = require('./lib/transport/events/events');
-module.exports.kafka = require('./lib/transport/events/kafka');
+module.exports.events = require('./lib/events');
+module.exports.kafka = require('./lib/events/provider/kafka');
