e260edd4001d4510605b36b5a1b104f53b468901
Marcel Hauf <marcel.hauf@n-fuse.de>
2016-06-01 16:38:55 +0200

    Replace gss with arango provider

--- test/database_test.js
+++ test/database_test.js
@@ -8,35 +8,86 @@ var assert = require('assert');
 var should = require('should');
 var util = require('util');
 var co = require('co');
+var _ = require('lodash');
 var isGenerator = require('is-generator');
 var isGeneratorFn = require('is-generator').fn
+
+var Arangojs = require('arangojs');
+
 var config = require('../lib/config');
 var database = require('../lib/database');
 
 var providers = [{
-  name: 'gss',
+  name: 'arango',
+  init: function(cb){
+    config.load(process.cwd() + '/test');
+    let cfg = config.get();
+    let dbHost = cfg.get('database:arango:host');
+    let dbPort = cfg.get('database:arango:port');
+    let dbName = cfg.get('database:arango:database');
+    let db = new Arangojs('http://' + dbHost + ':' + dbPort);
+    db.dropDatabase(dbName).then(function(result){
+      if (result.error) {
+        cb(result.error);
+        return;
+      }
+      cb();
+    }).catch(function(err){
+      if (err.message === 'database not found') {
+        cb();
+        return;
+      }
+      cb(err);
+    });
+  },
   loadInvalidConfig: function(){
     config.load(process.cwd() + '/test');
     let cfg = config.get();
-    cfg.set('database:gss:database', 'database_does_not_exist');
+    cfg.set('database:arango:autoCreate', false);
+    cfg.set('database:arango:database', 'database_does_not_exist');
   }
 }];
 let logger = {
-  log: function() {
-    let level = arguments[0].toLowerCase();
-    if (level == 'error') {
-      let args = Array.prototype.splice.apply(arguments, [1]);
-      console.log(level, args);
-    }
-  },
+  log: function() {},
 };
 providers.forEach(function(providerCfg) {
+  before(function(done){
+    providerCfg.init(done);
+  });
   describe('calling database.get', function() {
     context(util.format('with database provider %s', providerCfg.name), function() {
-      config.load(process.cwd() + '/test');
-      it('should return a database connection', function*() {
-        let db = yield database.get(providerCfg.name, logger);
-        should.exist(db);
+      context('and valid configuration', function(){
+        let collection = 'test';
+        let document = {
+          id: '/test/test',
+        };
+        let db;
+        config.load(process.cwd() + '/test');
+        it('should return a database connection', function*() {
+          db = yield database.get(providerCfg.name, logger);
+          should.exist(db);
+        });
+        describe('inserting a document', function(){
+          it('should store a document', function*(){
+            yield db.insert(collection, document);
+          });
+          it('should be findable', function*(){
+            let result = yield db.findOne(collection, document.id);
+            result.should.deepEqual(document);
+          });
+          it('should be updatable', function*(){
+            let newDoc = _.clone(document);
+            newDoc.value = 'new';
+            yield db.updateOne(collection, document.id, newDoc);
+            let result = yield db.findOne(collection, document.id);
+            result.should.deepEqual(newDoc);
+          });
+          it('should be deletable', function*(){
+            yield db.deleteOne(collection, document.id);
+            let result = yield db.findOne(collection, document.id);
+            should.not.exist(result);
+          });
+        });
       });
       context('and invalid configuration', function(){
         it('should throw an error', function*(){
