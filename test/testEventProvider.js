26f5e3bf08042fe41e94c1568160eb573a7bab6c
Marcel Hauf <marcel.hauf@n-fuse.de>
2016-06-07 16:39:21 +0200

    Add WIP event provider mockup

---
+++ test/testEventProvider.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const co = require('co');
+const _ = require('lodash');
+
+function Topic() {
+  
+}
+
+
+
+function Stack() {}
+
+Stack.prototype.topic = function* topic(name) {
+
+};
+
+module.exports = Stack;
