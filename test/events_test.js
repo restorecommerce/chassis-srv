eb5bd88ff224048b46186bc754b821adb1ae27a0
Marcel Hauf <marcel.hauf@n-fuse.de>
2016-06-08 11:48:30 +0200

    Add Kafka Topic listenerCount, removeListener, removeAllListeners

--- test/events_test.js
+++ test/events_test.js
@@ -55,12 +55,20 @@ describe('events', () => {
         should.exist(topic);
         should.exist(topic.on);
         should.exist(topic.emit);
+        should.exist(topic.listenerCount);
+        should.exist(topic.removeListener);
+        should.exist(topic.removeAllListeners);
+        should.ok(isGeneratorFn(topic.on));
         should.ok(isGeneratorFn(topic.emit));
+        should.ok(isGeneratorFn(topic.listenerCount));
+        should.ok(isGeneratorFn(topic.removeListener));
+        should.ok(isGeneratorFn(topic.removeAllListeners));
       });
     });
     describe('yielding kafka.start', () => {
       let callback;
-      const listener = function* makeListener(message) {
+      const listener = function* listener(message) {
+        should.exist(message);
         testMessage.value.should.equal(message.value);
         testMessage.count.should.equal(message.count);
         if (callback) {
@@ -73,13 +81,29 @@ describe('events', () => {
       });
       it('should allow listening to events', function* listenToEvents() {
         yield topic.on(eventName, listener);
+        yield topic.removeListener(eventName, listener);
       });
-      it('should allow sending', function* sendEvents(done) {
+      it('should allow removing all listeners', function* removeAllListeners() {
+        yield topic.on(eventName, listener);
+        yield topic.removeAllListeners(eventName);
+      });
+      it('should allow removing a listener', function* removeListener() {
+        yield topic.on(eventName, listener);
+        yield topic.removeListener(eventName, listener);
+      });
+      it('should allow counting listeners', function* countListeners() {
+        yield topic.on(eventName, listener);
+        const count = yield topic.listenerCount(eventName);
+        should.exist(count);
+        count.should.be.equal(1);
+        yield topic.removeListener(eventName, listener);
+      });
+      it('should allow emitting', function* sendEvents(done) {
+        yield topic.on(eventName, listener);
         callback = done;
         try {
           yield topic.emit(eventName, testMessage);
         } catch (e) {
-          console.log('error', e),
           done(e);
         }
       });
