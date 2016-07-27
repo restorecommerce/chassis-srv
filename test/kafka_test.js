3cb668c84b3b3d553b21015396f2f4e7aeae5e1c
Marcel Hauf <marcel.hauf@n-fuse.de>
2016-07-27 09:28:47 +0200

    Start/Stop Events only once per test

--- test/kafka_test.js
+++ test/kafka_test.js
@@ -11,27 +11,25 @@ const chassis = require('../');
 const config = chassis.config;
 const Events = chassis.events.Events;
 
-/* global describe it beforeEach afterEach */
+/* global describe it before after */
 
 describe('Kafka events provider', () => {
   let events;
-  beforeEach(function* setupProvider() {
+  before(function* setupProvider() {
     config.load(process.cwd() + '/test', logger);
     events = new Events('kafkaTest');
     yield events.start();
   });
-  afterEach(function* stopProvider() {
+  after(function* stopProvider() {
     yield events.end();
   });
   describe('topic.$wait', function testWait() {
-    this.timeout(10000);
+    this.timeout(5000);
     it('should wait until the event message is processed', function* waitUntil() {
       const testMessage = {
         value: 'test',
         count: 1,
       };
-      // works with topic: test.wait
-      // does not work with topic: test
       const topic = yield events.topic('test.wait');
       let receivedOffset = yield topic.$offset(-1);
       yield topic.on('test-event', function* onTestEvent(message, context) {
