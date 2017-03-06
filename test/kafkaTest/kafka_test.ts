47e22f636e2d296b3f0b5fc63de8a78776efbf80
akumar <arun.kumar@n-fuse.de>
2017-03-06 16:29:23 +0100

    kafka test type script.

---
+++ test/kafkaTest/kafka_test.ts
@@ -0,0 +1,45 @@
+'use strict';
+
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+const mocha = require('mocha');
+const coMocha = require('co-mocha');
+
+coMocha(mocha);
+
+const should = require('should');
+const logger = require('./../logger_test.js');
+const chassis = require('../../');
+
+const config = chassis.config;
+const Events = chassis.events.Events;
+
+/* global describe it before after */
+/*  eslint-disable require-yield */
+describe('Kafka events provider', () => {
+  let events;
+  before(function* setupProvider() {
+    yield config.load(process.cwd() + '/test', logger);
+    const cfg = yield chassis.config.get();
+    events = new Events(cfg.get('events:kafkaTest'), logger);
+    yield events.start();
+  });
+  after(function* stopProvider() {
+    yield events.end();
+  });
+  describe('topic.$wait', function testWait() {
+    this.timeout(5000);
+    it('should wait until the event message is processed', function* waitUntil() {
+      const testMessage = { value: 'value', count: 1 };
+      const topic = yield events.topic('test.wait');
+      let receivedOffset = yield topic.$offset(-1);
+      yield topic.on('test-event', function* onTestEvent(message, context) {
+        should.exist(message);
+        receivedOffset = context.offset;
+      });
+      const offset = yield topic.$offset(-1);
+      yield topic.emit('test-event', testMessage);
+      yield topic.$wait(offset);
+      offset.should.equal(receivedOffset);
+    });
+  });
+});
