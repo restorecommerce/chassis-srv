30dc96c49c9bc38747605e4078cef24029096c40
akumar <arun.kumar@n-fuse.de>
2017-06-20 16:31:12 +0200

    update

--- src/events/provider/kafka/index.ts
+++ src/events/provider/kafka/index.ts
@@ -9,6 +9,7 @@ import * as _ from "lodash";
 import * as EventEmitter from "co-emitter";
 // import * as root1 from "../../../../definitions/bundled";
 import * as protobuf from "protobufjs";
+import * as sleep from 'sleep';
 
 /**
  * A Kafka topic.
@@ -303,6 +304,9 @@ export class Topic {
       yield this.$subscribe();
     }
     this.emitter.on(eventName, listener);
+    // Fix for the failing test cases - since subscription to topic can
+    // take a while
+    sleep.sleep(2);
   }
 
   /**
