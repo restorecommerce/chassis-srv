1833333b6d9210432c5eb49f469481d3ff969e6f
akumar <arun.kumar@n-fuse.co>
2018-03-21 09:23:03 +0100

    middleware tests.

--- src/microservice/endpoint.ts
+++ src/microservice/endpoint.ts
@@ -5,7 +5,7 @@
  * @param middleware
  */
 export function chain(middleware: any): any {
-  return async function generator(next: any): Promise<any> {
+  return async function middlewareChain(next: any): Promise<any> {
     let n = next;
     for (let i = middleware.length - 1; i >= 1; i -= 1) {
       n = await middleware[i](n);
---
+++ test/middleware_test.ts
@@ -0,0 +1,48 @@
+'use strict';
+
+import * as should from 'should';
+import { endpoint as chain } from '../lib';
+
+/* global describe it*/
+
+function endpoint(request, context) {
+  return {
+    chain: context.chain,
+  };
+}
+
+function makeMiddleware(n) {
+  return function genMiddleware(next) {
+    return async function middleware(request, context) {
+      context.chain.push(n);
+      return await next(request, context);
+    };
+  };
+}
+
+describe('endpoint.chain', () => {
+  let middleware: any;
+  const tree = [];
+  let e;
+  it('should chain middleware', function checkChainMiddleware() {
+    const middlewares = [];
+    for (let i = 0; i < 5; i += 1) {
+      tree.push(i);
+      middlewares.push(makeMiddleware(i));
+    }
+    middleware = chain(middlewares);
+  });
+  it('should return an async function which gives an endpoint',
+    async function checkMiddlewareCreating() {
+      e = await middleware(endpoint);
+      should.exist(e);
+    });
+  it('should call middlewares in sequence from first to last', async function checkMiddlewareCalling() {
+    const result = await e({}, {
+      chain: [],
+    });
+    should.exist(result);
+    should.exist(result.chain);
+    result.chain.should.deepEqual(tree);
+  });
+});
